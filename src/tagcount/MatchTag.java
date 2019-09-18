0314022a868fde10a9ced7a4827c4c157a5f65ea
Shreya Pai <ayerhs1997@gmail.com>
2019-09-18 14:03:40 +0530

    Revert "Added a comment"
    This reverts commit 11a447fefde764dc57fd7c4209589d59f9ce86ee.

--- src/tagcount/MatchTag.java
+++ src/tagcount/MatchTag.java
@@ -7,7 +7,6 @@ import java.util.regex.Pattern;
 public class MatchTag {
     public String countTag(String line) 
     {   
-        //Pattern to match open html tag
         Matcher match = Pattern.compile("<[a-zA-Z]+").matcher(line);;
         while (match.find()) {
             return match.group();
